349ad333880deb32a59563d79330cc593f7c5364
Max Caulfield <admin@ulc.world>
2024-02-29 23:04:18 +0800

    modified:   CPP/local/8/random/main.cpp

--- CPP/local/8/random/main.cpp
+++ CPP/local/8/random/main.cpp
@@ -2,34 +2,49 @@
 
 #include <iostream>
 #include <random>
+#include <chrono>   // for std::chrono
 
-int * generateUniformRandomInt(int * numberList, int listSize);
-int * generateNormalRandomInt(int * numberList, int listSize);
+int * mt19937GetNormal(int * numberList, int listSize, std::mt19937 mt);
+int * mt19937GetAlternative(int * numberList, int listSize, std::mt19937 mt);
+int * randGet(int * numberList, int listSize);
 void outputInt(int * numberList, int listSize, int newlineCount);
 
 int main()
 {
-    constexpr int listSize {60};
-    constexpr int newlineCount {15};
+    constexpr int listSize {100000};
+    [[maybe_unused]] constexpr int newlineCount {15};
+    
+    // Only seed a PRNG once
+    // std::mt19937 mt {static_cast<std::mt19937::result_type>(std::chrono::steady_clock::now().time_since_epoch().count())};  // set seed using time. steady_clock has a more granular tick than time()
+    std::mt19937 mt {std::random_device{}()}; // ask the OS for a random number. caution: std::random_device**{}**(), both curly braces and parentheses. 
+    // Q: What does std::random_device{}() mean? // std::random_device{} creates a value-initialized temporary object of type std::random_device. The () then calls operator() on that temporary object, which returns a randomized value
+
+    // testing random num generator executing duration (testing chrono, seems fun)
 
     int uniformRandomInt[listSize] {};
-    generateUniformRandomInt(uniformRandomInt, listSize);
+    mt19937GetNormal(uniformRandomInt, listSize, mt);       // 1,236,600ns  slowest, interesting
 
     int normalRandomInt[listSize] {};
-    generateNormalRandomInt(normalRandomInt, listSize);
+    mt19937GetAlternative(normalRandomInt, listSize, mt);   // 440,400ns
 
+    int randRandomInt[listSize] {};
+    randGet(randRandomInt, listSize);                   // 1,021,200ns
+
+/*
     std::cout << "with uniform_int_distribution: \n";
     outputInt(uniformRandomInt, listSize, newlineCount);
 
     std::cout << "\nwithout uniform_int_distribution: \n";
     outputInt(normalRandomInt, listSize, newlineCount);
+*/
 
     return 0;
 }
 
-int * generateUniformRandomInt(int * numberList, int listSize)
+int * mt19937GetNormal(int * numberList, int listSize, std::mt19937 mt)
 {
-    std::mt19937 mt {};
+    const auto timeStart {std::chrono::steady_clock::now()};    // weird return type, use auto here. // https://en.cppreference.com/w/cpp/chrono/time_point
+
     std::uniform_int_distribution uniformRandomInt {0,9};
 
     for (int i = 0; i < listSize; i++)
@@ -37,18 +52,46 @@ int * generateUniformRandomInt(int * numberList, int listSize)
         numberList[i] = uniformRandomInt(mt);
     }
 
+    const auto timeEnd {std::chrono::steady_clock::now()};
+
+    std::cout << "Duration of mt19937_normal " << listSize << " numbers: " << (timeEnd - timeStart) << '\n';
+
     return (numberList);
 }
 
-int * generateNormalRandomInt(int * numberList, int listSize)
+int * mt19937GetAlternative(int * numberList, int listSize, std::mt19937 mt)
 {
-    std::mt19937 mt {};
+    const auto timeStart {std::chrono::steady_clock::now()};    // weird return type, use auto here. // https://en.cppreference.com/w/cpp/chrono/time_point
+
     // std::uniform_int_distribution uniformRandomInt {0,9};
 
     for (int i = 0; i < listSize; i++)
     {
         numberList[i] = mt() % 10;
     }
+
+    const auto timeEnd {std::chrono::steady_clock::now()};
+
+    std::cout << "Duration of mt19937_alt " << listSize << " numbers: " << (timeEnd - timeStart) << '\n';
+
+    return (numberList);
+}
+
+int * randGet(int * numberList, int listSize)   // rand() is typically used in C
+{
+    const auto timeStart {std::chrono::steady_clock::now()};    // weird return type, use auto here. // https://en.cppreference.com/w/cpp/chrono/time_point
+
+    std::srand(std::random_device{}());
+    // std::uniform_int_distribution uniformRandomInt {0,9};
+
+    for (int i = 0; i < listSize; i++)
+    {
+        numberList[i] = std::rand() % 10;
+    }
+
+    const auto timeEnd {std::chrono::steady_clock::now()};
+
+    std::cout << "Duration of rand " << listSize << " numbers: " << (timeEnd - timeStart) << '\n';
 
     return (numberList);
 }
