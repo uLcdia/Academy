f2a3bff39d84d91456e5c240b2ead77f0a6544af
Max Caulfield <admin@ulc.world>
2024-03-16 16:43:51 +0800

    Add example of access function and its return

---
+++ CPP/local/14/accessFunction/accessFunction.cpp
@@ -0,0 +1,37 @@
+// https://www.learncpp.com/cpp-tutorial/member-functions-returning-references-to-data-members/
+
+#include <iostream>
+#include <string>
+
+class Foo
+{
+private:
+    std::string m_name {};
+
+public:
+    void setName(std::string_view name) { m_name = name; }
+
+    // Returning a std::string_view would require a temporary std::string_view to be created and returned every time the function was called.
+    // don't replace std::string& with auto&
+    // don't remove the first const, otherwise some reference below might be able to change m_name directly
+    // don't remove the second const, otherwise const Foo won't be able to use getName()
+    const std::string& getName() const { return m_name; }   // const ... const
+};
+
+int main()
+{
+    Foo foo;
+    
+    foo.setName("Aigis");
+
+    std::string bar {foo.getName()};
+    bar = "Koromaru";
+
+    // const std::string& goo {foo.getName()};  // don't do this
+    // std::string& goo {foo.getName()};   // qualifiers dropped in binding reference of type "std::string &" to initializer of type "const std::string"    // don't do this
+
+    std::cout << "foo: " << foo.getName() << '\n';
+    std::cout << "bar: " << bar << '\n';
+
+    return 0;
+}
