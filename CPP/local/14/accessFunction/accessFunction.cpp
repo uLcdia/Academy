b2164efdf8fb843f273a3cf092dd2123dfd8c436
Max Caulfield <admin@ulc.world>
2024-03-18 20:40:44 +0800

    modified:   CPP/local/14/accessFunction/accessFunction.cpp

--- CPP/local/14/accessFunction/accessFunction.cpp
+++ CPP/local/14/accessFunction/accessFunction.cpp
@@ -5,9 +5,6 @@
 
 class Foo
 {
-private:
-    std::string m_name {};
-
 public:
     void setName(std::string_view name) { m_name = name; }
 
@@ -16,6 +13,12 @@ public:
     // don't remove the first const, otherwise some reference below might be able to change m_name directly
     // don't remove the second const, otherwise const Foo won't be able to use getName()
     const std::string& getName() const { return m_name; }   // const ... const
+
+private:
+    std::string m_name {};
+
+    // Declare public members first, protected members next, and private members last. This spotlights the public interface and de-emphasizes implementation details.
+
 };
 
 int main()
@@ -28,7 +31,7 @@ int main()
     bar = "Koromaru";
 
     // const std::string& goo {foo.getName()};  // don't do this
-    // std::string& goo {foo.getName()};   // qualifiers dropped in binding reference of type "std::string &" to initializer of type "const std::string"    // don't do this
+    // std::string& goo {foo.getName()};   // error: qualifiers dropped in binding reference of type "std::string &" to initializer of type "const std::string"    // don't do this
 
     std::cout << "foo: " << foo.getName() << '\n';
     std::cout << "bar: " << bar << '\n';
