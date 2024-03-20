cce63435c6d90cd273f322eaff88d4a762e35908
Max Caulfield <admin@ulc.world>
2024-03-21 00:00:17 +0800

    Add example of copy constructor

---
+++ CPP/local/14/copyConstructor/copyConstructor.cpp
@@ -0,0 +1,55 @@
+// https://www.learncpp.com/cpp-tutorial/introduction-to-the-copy-constructor/
+
+#include <iostream>
+
+class Fraction
+{
+public:
+    Fraction(int numerator = 0, int denominator = 1)
+        : m_numerator {numerator}
+        , m_denominator {denominator}
+    {}
+
+    Fraction(const Fraction& fraction) // if const Fraction fraction: error: copy constructor must pass its first argument by reference
+        : m_numerator {fraction.m_numerator}
+        , m_denominator {fraction.m_denominator}
+    // Access controls work on a per-class basis (not a per-object basis). This means the member functions of a class can access the private members of any class object of the same type (not just the implicit object).
+    {
+        std::cout << "COPY CONSTRUCTOR\n";
+    }  // Copy constructors should have no side effects beyond copying.
+
+    void print() const {std::cout << "print(): Fraction(" << m_numerator << ", " << m_denominator << ")\n";}
+
+
+private:
+    int m_numerator {0};
+    int m_denominator {1};
+};
+
+void printFraction(Fraction f)
+{
+    f.print();
+}
+
+Fraction generateFraction(int numerator, int denominator)
+{
+    Fraction f {numerator, denominator};
+    return f;   // optimized: When generateFraction returns a Fraction back to main, the copy constructor is implicitly called again.
+}
+
+int main()
+{
+    std::cout << "Fraction f1 {10, 20};\n";
+    Fraction f1 {10, 20};
+
+    std::cout << "printFraction(f1);\n";
+    printFraction(f1);
+
+    std::cout << "Fraction f2 {generateFraction(30, 40)};\n";
+    Fraction f2 {generateFraction(30, 40)};
+
+    std::cout << "printFraction(f2);\n";
+    printFraction(f2);
+
+    return 0;
+}
