f2c2e4ab081a3a94c414efc6b35b7180951ea664
Max Caulfield <admin@ulc.world>
2024-04-24 22:11:54 +0800

    modified:   CPP/local/18/time/time.cpp

--- CPP/local/18/time/time.cpp
+++ CPP/local/18/time/time.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <chrono>
 #include <numeric>
+#include <string>
 
 void sortAlgorithm()
 {
@@ -48,6 +49,15 @@ private:
     std::chrono::time_point<Clock, Second> m_begin {Clock::now()};
 };
 
+void printString(std::string string)
+{
+    std::cout << string << std::endl;
+}
+void printStringView(std::string_view string)
+{
+    std::cout << string << std::endl;
+}
+
 int main()
 {
     Timer t {};
@@ -57,5 +67,16 @@ int main()
     t.reset();
     sortBubble();
     t.printElapsed();
+
+    std::string s {"I've seen things you people wouldn't believe... Attack ships on fire off the shoulder of Orion... I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain... Time to die."};
+
+    t.reset();
+    printString(s);
+    t.printElapsed();
+
+    t.reset();
+    printStringView(s);
+    t.printElapsed();
+
     return 0;
 }
