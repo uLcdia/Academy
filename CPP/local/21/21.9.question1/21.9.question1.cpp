5908d837885960e360952f53cbc42c18f88993d4
Max Caulfield <admin@ulc.world>
2024-04-28 09:04:51 +0800

    modified:   CPP/local/21/21.9.question1/21.9.question1.cpp std::map implementation

--- CPP/local/21/21.9.question1/21.9.question1.cpp
+++ CPP/local/21/21.9.question1/21.9.question1.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <map>
 
 struct StudentGrade
 {
@@ -32,15 +33,28 @@ char& GradeMap::operator[] (std::string_view name)
     return m_map.back().grade;
 }
 
+// https://www.geeksforgeeks.org/map-vs-unordered_map-c/
+// https://en.cppreference.com/w/cpp/container/unordered_map
+// https://en.cppreference.com/w/cpp/container/map
+std::map<std::string, char> map {};
+
 int main()
 {
+    // custom implementation GradeMap:
     GradeMap grades{};
 
 	grades["Joe"] = 'A';
 	grades["Frank"] = 'B';
 
-	std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
-	std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
-    
+	std::cout << "Joe has a grade of " << grades["Joe"] << std::endl;
+	std::cout << "Frank has a grade of " << grades["Frank"] << std::endl;
+
+    // std::map map:
+    map.emplace("Max", 'A');
+    map.emplace("Kotone", 'A');
+
+	std::cout << "Max has a grade of " << map["Max"] << std::endl;
+	std::cout << "Kotone has a grade of " << map["Kotone"] << std::endl;
+
     return 0;
 }
