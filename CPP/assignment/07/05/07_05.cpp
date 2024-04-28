56733d29271a9835705bad40ca2068a7a7ee5c7b
Max Caulfield <admin@ulc.world>
2024-04-28 11:17:44 +0800

    Assignment 07_05

---
+++ CPP/assignment/07/05/07_05.cpp
@@ -0,0 +1,28 @@
+// class Mammal as base, class Dog as derived
+
+#include <iostream>
+
+class Mammal
+{
+public:
+    Mammal(int foo) : m_foo {foo} {std::cout << "constructing Mammal." << std::endl;}
+    ~Mammal() {std::cout << "destructing Mammal." << std::endl;}
+private:
+    [[maybe_unused]]int m_foo {};
+};
+
+class Dog : public Mammal
+{
+public:
+    Dog(int foo, int bar) : Mammal {foo}, m_bar {bar} {std::cout << "constructing Dog." << std::endl;}
+    ~Dog() {std::cout << "destructing Dog." << std::endl;}
+private:
+    [[maybe_unused]] int m_bar {};
+};
+
+int main()
+{
+    Dog wolfie {0, 1};
+
+    return 0;
+}
